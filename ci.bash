f41ab4eeddefa72a372c8f790feccae5de7e2bb2
Sylvain Hellegouarch <sh@defuze.org>
2017-10-02 14:32:44 +0200

    Some visibility is good

--- ci.bash
+++ ci.bash
@@ -2,6 +2,7 @@
 set -eo pipefail
 
 function build-docs () {
+    echo "Building the documentation"
     origin=$PWD
     mkdir /tmp/site
     cd /tmp/site
@@ -15,6 +16,7 @@ function build-docs () {
 }
 
 function publish-docs () {
+    echo "Publishing the documentation"
     origin=$PWD
     cd /tmp/site
     echo "chaostoolkit.org" > CNAME
@@ -25,28 +27,35 @@ function publish-docs () {
 }
 
 function lint () {
+    echo "Checking the code syntax"
     pycodestyle --first chaostoolkit
 }
 
 function build () {
+    echo "Building the choastoolkit package"
     python setup.py build
 }
 
 function run-test () {
+    echo "Running the tests"
     python setup.py test
 }
 
 function release () {
+    echo "Releasing the pachaage"
     python setup.py release
 
+    echo "Publishing to PyPI"
     pip install twine
     twine upload dist/* -u ${PYPI_USER_NAME} -p ${PYPI_PWD}
 
+    echo "Building the Docker image"
     docker login -u ${DOCKER_USER_NAME} -p ${DOCKER_PWD}
     docker build -t chaostoolkit/chaostoolkit .
 
     docker tag chaostoolkit/chaostoolkit:latest chaostoolkit/chaostoolkit:$TRAVIS_TAG
     
+    echo "Publishing to the Docker repository"
     docker push chaostoolkit/chaostoolkit:$TRAVIS_TAG
     docker push chaostoolkit/chaostoolkit:latest
 }
@@ -57,16 +66,16 @@ function main () {
     run-test || return 1
 
     if [[ $TRAVIS_PYTHON_VERSION =~ ^3\.5+$ ]]; then
-        build-docs || return 1
+        build-docs || return 1
 
-        if [[ $TRAVIS_PULL_REQUEST == false ]]; then
+        if [[ $TRAVIS_PULL_REQUEST == false && $TRAVIS_BRANCH == 'master' ]]; then
             # build docs on each commit but only from master
-            publish-docs || return 1
+            publish-docs || return 1
         fi
 
         if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
             echo "Releasing tag $TRAVIS_TAG with Python $TRAVIS_PYTHON_VERSION"
-            release || return 1
+            release || return 1
         fi
     fi
 }
