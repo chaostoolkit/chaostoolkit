4e17dbbba6f313488aa71d452e35c306ac6bb06b
Sylvain Hellegouarch <sh@defuze.org>
2018-08-27 20:14:54 +0200

    Add JSON logging format
    Contributes to #76
    
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- chaostoolkit/__init__.py
+++ chaostoolkit/__init__.py
@@ -1,2 +1,27 @@
 # -*- coding: utf-8 -*-
+from datetime import date, datetime
+import decimal
+import uuid
+
 __version__ = '1.2.0'
+__all__ = ['__version__', 'encoder']
+
+
+def encoder(o: object) -> str:
+    """
+    Perform some additional encoding for types JSON doesn't support natively.
+
+    We don't try to respect any ECMA specification here as we want to retain
+    as much information as we can.
+    """
+    if isinstance(o, (date, datetime)):
+        # we do not meddle with the timezone and assume the date was stored
+        # with the right information of timezone as +-HH:MM
+        return o.isoformat()
+    elif isinstance(o, decimal.Decimal):
+        return str(o)
+    elif isinstance(o, uuid.UUID):
+        return str(o)
+
+    raise TypeError(
+        "Object of type '{}' is not JSON serializable".format(type(o)))
---
+++ chaostoolkit/logging.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import logging
+import uuid
+
+import logzero
+from logzero import logger, LogFormatter, setup_default_logger
+from pythonjsonlogger import jsonlogger
+
+from chaostoolkit import encoder
+
+__all__ = ["configure_logger"]
+
+
+class ChaosToolkitContextFilter(logging.Filter):
+    def __init__(self, name: str = '', context_id: str = None):
+        logging.Filter.__init__(self, name)
+        self.context_id = context_id or str(uuid.uuid4())
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        record.context_id = self.context_id
+        return True
+
+
+def configure_logger(verbose: bool = False, log_format: str = "string",
+                     log_file: str = None, logger_name: str = "chaostoolkit",
+                     context_id: str = None):
+    """
+    Configure the chaostoolkit logger.
+
+    By default logs as strings to stdout and the given file. When `log_format`
+    is `"json"`, records are set to the console as JSON strings but remain
+    as strings in the log file. The rationale is that the log file is mostly
+    for grepping purpose while records written to the console can be forwarded
+    out of band to anywhere else.
+    """
+    log_level = logging.INFO
+    fmt = "%(color)s[%(asctime)s %(levelname)s]%(end_color)s %(message)s"
+    if verbose:
+        log_level = logging.DEBUG
+        fmt = "%(color)s[%(asctime)s %(levelname)s] "\
+              "[%(module)s:%(lineno)d]%(end_color)s %(message)s"
+
+    formatter = LogFormatter(fmt=fmt, datefmt="%Y-%m-%d %H:%M:%S")
+    if log_format == 'json':
+        fmt = "(process) (asctime) (levelname) (module) (lineno) (message)"
+        if context_id:
+            fmt = "(context_id) {}".format(fmt)
+        formatter = jsonlogger.JsonFormatter(
+            fmt, json_default=encoder, timestamp=True)
+
+    # sadly, no other way to specify the name of the default logger publicly
+    LOGZERO_DEFAULT_LOGGER = logger_name
+    logger = setup_default_logger(level=log_level, formatter=formatter)
+    if context_id:
+        logger.addFilter(ChaosToolkitContextFilter(logger_name, context_id))
+
+    if log_file:
+        # always everything as strings in the log file
+        logger.setLevel(logging.DEBUG)
+        fmt = "%(color)s[%(asctime)s %(levelname)s] "\
+              "[%(module)s:%(lineno)d]%(end_color)s %(message)s"
+        formatter = LogFormatter(fmt=fmt, datefmt="%Y-%m-%d %H:%M:%S")
+        logzero.logfile(log_file, formatter=formatter, mode='a',
+                        loglevel=logging.DEBUG)
