f12bf40635ddc47cad1df3adfcbcd3de6b0a4682
Sylvain Hellegouarch <sh@defuze.org>
2018-09-09 13:57:38 +0200

    Externalize dev reqs
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- setup.py
+++ setup.py
@@ -41,13 +41,9 @@ packages = [
 
 needs_pytest = set(['pytest', 'test']).intersection(sys.argv)
 pytest_runner = ['pytest_runner'] if needs_pytest else []
-test_require = [
-    'coverage',
-    'pytest>=2.8',
-    'pytest-cov',
-    'pytest-sugar',
-    'semver',
-]
+test_require = []
+with io.open('requirements-dev.txt') as f:
+    test_require = [l.strip() for l in f if not l.startswith('#')]
 
 install_require = []
 with io.open('requirements.txt') as f:
