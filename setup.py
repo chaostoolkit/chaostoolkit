c0f9177c2ad7418670f1d16d582d160ca1311f3a
Sylvain Hellegouarch <sh@defuze.org>
2018-09-09 13:41:07 +0200

    Notify Python 3.7 support
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ classifiers = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: Implementation',
     'Programming Language :: Python :: Implementation :: CPython',
     'Topic :: System :: Distributed Computing'
