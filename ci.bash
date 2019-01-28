d33a4e07d147d52edc0143b4e6b1538ea5f12f13
Sylvain Hellegouarch <sh@defuze.org>
2019-01-28 18:18:34 +0100

    Build for macosx
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- ci.bash
+++ ci.bash
@@ -58,10 +58,12 @@ function main () {
     build || return 1
     run-test || return 1
 
-    if [[ $TRAVIS_PYTHON_VERSION =~ ^3\.5+$ ]]; then
-        if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+(rc[0-9]+)?$ ]]; then
-            echo "Releasing tag $TRAVIS_TAG with Python $TRAVIS_PYTHON_VERSION"
-            release || return 1
+    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+        if [[ $TRAVIS_PYTHON_VERSION =~ ^3\.5+$ ]]; then
+            if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+(rc[0-9]+)?$ ]]; then
+                echo "Releasing tag $TRAVIS_TAG with Python $TRAVIS_PYTHON_VERSION"
+                release || return 1
+            fi
         fi
     fi
 }
