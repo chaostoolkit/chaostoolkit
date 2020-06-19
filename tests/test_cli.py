e057452455b617f6161886c22753922e2261ab6e
Sylvain Hellegouarch <sh@defuze.org>
2020-06-19 15:42:57 +0200

    Add settings get/set/remove/show commands (#179)
    Contributes to #65
    
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,7 @@ from decimal import Decimal
 import io
 import json
 import os
+import shutil
 import sys
 import tempfile
 from unittest.mock import ANY, patch
@@ -16,6 +17,7 @@ from chaoslib.settings import CHAOSTOOLKIT_CONFIG_PATH
 import click
 from click.testing import CliRunner
 import pytest
+import yaml
 
 from chaostoolkit.cli import cli, encoder
 
@@ -302,3 +304,261 @@ def test_notify_init_complete(notify):
 
         notify.assert_any_call(ANY, InitFlowEvent.InitStarted)
         notify.assert_any_call(ANY, InitFlowEvent.InitCompleted, ANY)
+
+
+def test_show_settings():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as d:
+        settings_path = os.path.join(d, 'settings.yaml')
+        shutil.copy(
+            os.path.join(
+                os.path.dirname(__file__), 'fixtures',
+                'complete-settings.yaml'), settings_path)
+        settings_content = open(settings_path).read()
+        result = runner.invoke(
+            cli, ['--settings', settings_path, 'settings', 'show'])
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert yaml.dump(yaml.load(result.output), canonical=True) == \
+            yaml.dump(yaml.load(settings_content), canonical=True)
+
+
+def test_get_settings_entry_as_yaml():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as d:
+        settings_path = os.path.join(d, 'settings.yaml')
+        shutil.copy(
+            os.path.join(
+                os.path.dirname(__file__), 'fixtures',
+                'complete-settings.yaml'), settings_path)
+        settings_content = open(settings_path).read()
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'get',
+                'auths.chaos\\\\.example\\\\.com:8443.type'
+            ]
+        )
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert result.output == "bearer\n...\n\n"
+
+
+def test_get_settings_entry_as_json():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as d:
+        settings_path = os.path.join(d, 'settings.yaml')
+        shutil.copy(
+            os.path.join(
+                os.path.dirname(__file__), 'fixtures',
+                'complete-settings.yaml'), settings_path)
+        settings_content = open(settings_path).read()
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'get',
+                '--format=json',
+                'auths.chaos\\\\.example\\\\.com:8443.type'
+            ]
+        )
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert result.output == '"bearer"\n'
+
+
+def test_get_settings_entry_as_string():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as d:
+        settings_path = os.path.join(d, 'settings.yaml')
+        shutil.copy(
+            os.path.join(
+                os.path.dirname(__file__), 'fixtures',
+                'complete-settings.yaml'), settings_path)
+        settings_content = open(settings_path).read()
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'get',
+                '--format=string',
+                'auths.chaos\\\\.example\\\\.com:8443.type'
+            ]
+        )
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert result.output == 'bearer\n'
+
+
+def test_get_settings_not_found_key_exits_with_1():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as d:
+        settings_path = os.path.join(d, 'settings.yaml')
+        shutil.copy(
+            os.path.join(
+                os.path.dirname(__file__), 'fixtures',
+                'complete-settings.yaml'), settings_path)
+        settings_content = open(settings_path).read()
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'get',
+                'burp'
+            ]
+        )
+        assert result.exit_code == 1
+
+
+def test_set_settings_not_found_key_exits_with_1():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as d:
+        settings_path = os.path.join(d, 'settings.yaml')
+        shutil.copy(
+            os.path.join(
+                os.path.dirname(__file__), 'fixtures',
+                'complete-settings.yaml'), settings_path)
+        settings_content = open(settings_path).read()
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'set',
+                'burp', '"hello"'
+            ]
+        )
+        assert result.exit_code == 1
+
+
+def test_set_settings_entry():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as d:
+        settings_path = os.path.join(d, 'settings.yaml')
+        shutil.copy(
+            os.path.join(
+                os.path.dirname(__file__), 'fixtures',
+                'complete-settings.yaml'), settings_path)
+        settings_content = open(settings_path).read()
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'set',
+                'auths.chaos\\\\.example\\\\.com:8443', '"basic"'
+            ]
+        )
+        assert result.exit_code == 0
+    
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'get',
+                '--format=json',
+                'auths.chaos\\\\.example\\\\.com:8443'
+            ]
+        )
+        assert result.exit_code == 0
+        assert result.output == '"basic"\n'
+
+
+def test_set_settings_entry_as_a_list():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as d:
+        settings_path = os.path.join(d, 'settings.yaml')
+        shutil.copy(
+            os.path.join(
+                os.path.dirname(__file__), 'fixtures',
+                'complete-settings.yaml'), settings_path)
+        settings_content = open(settings_path).read()
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'set',
+                'auths.chaos\\\\.example\\\\.com:8443', '["basic", "bearer"]'
+            ]
+        )
+        assert result.exit_code == 0
+    
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'get',
+                '--format=json',
+                'auths.chaos\\\\.example\\\\.com:8443'
+            ]
+        )
+        assert result.exit_code == 0
+        assert json.loads(result.output) == ["basic", "bearer"]
+
+
+def test_set_settings_entry_as_a_int():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as d:
+        settings_path = os.path.join(d, 'settings.yaml')
+        shutil.copy(
+            os.path.join(
+                os.path.dirname(__file__), 'fixtures',
+                'complete-settings.yaml'), settings_path)
+        settings_content = open(settings_path).read()
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'set',
+                'auths.chaos\\\\.example\\\\.com:8443', '3'
+            ]
+        )
+        assert result.exit_code == 0
+    
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'get',
+                '--format=json',
+                'auths.chaos\\\\.example\\\\.com:8443'
+            ]
+        )
+        assert result.exit_code == 0
+        assert json.loads(result.output) == 3
+
+
+def test_remove_settings_not_found_key_exits_with_1():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as d:
+        settings_path = os.path.join(d, 'settings.yaml')
+        shutil.copy(
+            os.path.join(
+                os.path.dirname(__file__), 'fixtures',
+                'complete-settings.yaml'), settings_path)
+        settings_content = open(settings_path).read()
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'remove',
+                'burp'
+            ]
+        )
+        assert result.exit_code == 1
+
+
+def test_remove_settings_entry():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as d:
+        settings_path = os.path.join(d, 'settings.yaml')
+        shutil.copy(
+            os.path.join(
+                os.path.dirname(__file__), 'fixtures',
+                'complete-settings.yaml'), settings_path)
+        settings_content = open(settings_path).read()
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'remove',
+                'auths.chaos\\\\.example\\\\.com:8443'
+            ]
+        )
+        assert result.exit_code == 0
+    
+        result = runner.invoke(
+            cli,
+            [
+                '--settings', settings_path, 'settings', 'get',
+                '--format=json',
+                'auths.chaos\\\\.example\\\\.com:8443'
+            ]
+        )
+        assert result.exit_code == 1
