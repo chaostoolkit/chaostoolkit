fbe0b9323329ad433ae72545b2633f89498bfb13
Sylvain Hellegouarch <sh@defuze.org>
2021-02-16 12:09:50 +0100

    Moving to setup.cfg
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 """chaostoolkit builder and installer"""
+import os.path
 
-import sys
-import io
-from os.path import abspath, dirname, join, normpath
-
-import setuptools
+from setuptools import setup
 
 
 def get_version_from_package() -> str:
     """
     Read the package version from the source without importing it.
     """
-    path = join(dirname(__file__), "chaostoolkit/__init__.py")
-    path = normpath(abspath(path))
+    path = os.path.join(os.path.dirname(__file__), "chaostoolkit/__init__.py")
+    path = os.path.normpath(os.path.abspath(path))
     with open(path) as f:
         for line in f:
             if line.startswith("__version__"):
@@ -22,73 +19,8 @@ def get_version_from_package() -> str:
                 return version
 
 
-name = 'chaostoolkit'
-desc = 'Chaos Engineering Toolkit'
-
-with io.open('README.md', encoding='utf-8') as strm:
-    long_desc = strm.read()
-
-classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research',
-    'Intended Audience :: System Administrators',
-    'Operating System :: OS Independent',
-    'License :: OSI Approved :: Apache Software License',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: Implementation',
-    'Programming Language :: Python :: Implementation :: CPython',
-    'Topic :: System :: Distributed Computing',
-    'Topic :: Utilities'
-]
-author = 'Chaos Toolkit Team'
-author_email = 'contact@chaostoolkit.org'
-url = 'https://chaostoolkit.org'
-license = 'Apache Software License 2.0'
-packages = [
-    'chaostoolkit'
-]
-
-needs_pytest = set(['pytest', 'test']).intersection(sys.argv)
-pytest_runner = ['pytest_runner'] if needs_pytest else []
-test_require = []
-with io.open('requirements-dev.txt') as f:
-    test_require = [l.strip() for l in f if not l.startswith(('#', '-e'))]
-
-install_require = []
-with io.open('requirements.txt') as f:
-    install_require = [l.strip() for l in f if not l.startswith(('#', '-e'))]
-
-setup_params = dict(
-    name=name,
-    version=get_version_from_package(),
-    description=desc,
-    long_description=long_desc,
-    long_description_content_type='text/markdown',
-    classifiers=classifiers,
-    author=author,
-    author_email=author_email,
-    url=url,
-    license=license,
-    packages=packages,
-    entry_points={'console_scripts': ['chaos = chaostoolkit.__main__:cli']},
-    include_package_data=True,
-    install_requires=install_require,
-    tests_require=test_require,
-    setup_requires=pytest_runner,
-    python_requires='>=3.5.*'
-)
-
-
-def main():
-    """Package installation entry point."""
-    setuptools.setup(**setup_params)
-
-
 if __name__ == '__main__':
-    main()
+    setup(
+        name="chaostoolkit",
+        version=get_version_from_package()
+    )
